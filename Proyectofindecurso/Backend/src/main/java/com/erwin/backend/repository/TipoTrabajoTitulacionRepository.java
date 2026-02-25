d754ffb9302a3e41933c0b1312ee8f3dd56c4c59
Troyano2004 <erwinbueno05@gmail.com>
2026-02-25 17:09:22 -0500

    aja

--- Proyectofindecurso/Backend/src/main/java/com/erwin/backend/repository/TipoTrabajoTitulacionRepository.java
+++ Proyectofindecurso/Backend/src/main/java/com/erwin/backend/repository/TipoTrabajoTitulacionRepository.java
@@ -2,9 +2,12 @@ package com.erwin.backend.repository;
 
 import com.erwin.backend.entities.Tipotrabajotitulacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TipoTrabajoTitulacionRepository extends JpaRepository<Tipotrabajotitulacion,Integer> {
     List<Tipotrabajotitulacion> findByModalidadTitulacion_IdModalidad(Integer modalidadTitulacionIdModalidad);
+    List<Tipotrabajotitulacion> findByModalidadTitulacionIdModalidad(Integer idModalidad);
 }
